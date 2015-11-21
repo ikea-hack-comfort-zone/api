69408766829f4445f5044b32efa61383e7e40a11
Stephan Broek <SABroek1@shef.ac.uk>
2015-11-21 04:39:22 +0100

    added post endpoint and made sure everything works

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,14 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+router.post('/submit' ,function(req, res) {
+  var sensor = req.body.sensor;
+  var temp = req.body.temp;
+  var mic = req.body.mic;
+  var light = req.body.light;
+
+  var ret = {temp: temp, mic: mic, light: light};
+  res.json(ret);
+});
+
 module.exports = router;
