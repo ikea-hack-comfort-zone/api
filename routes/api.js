f14c343adeac1a8ede8336458031bf1f6aa053d8
BT@ikeahack <benjamin.tyler+ikeahack@gmail.com>
2015-11-21 04:04:54 -0500

    Add route to fetch sensor data + CORS

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+// TODO: shove into DB
 router.post('/submit' ,function(req, res) {
   var sensor = req.body.sensor;
   var temp = req.body.temp;
@@ -9,6 +10,12 @@ router.post('/submit' ,function(req, res) {
   var light = req.body.light;
 
   var ret = {temp: temp, mic: mic, light: light};
+  res.json(ret);
+});
+
+// TODO: pull from DB
+router.get('/sensor-data', function(req, res) {
+  var ret = {temp: 50 * Math.random(), mic: 120 * Math.random(), light: 15 * Math.random()};
   res.json(ret);
 });
 
